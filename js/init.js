cc4d77660bafa5e9734143431184d7686cb6bafc
evaluaciontercerparcial801JRV <evaluaciontercerparcial801JRV@gmail.com>
2021-08-01 18:56:03 -0500

    Cambios

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCwOmnDA5KBybeb03ERpJciZazr9XpvpwY",
+    authDomain: "bdwp801.firebaseapp.com",
+    projectId: "bdwp801",
+    storageBucket: "bdwp801.appspot.com",
+    messagingSenderId: "101167042392",
+    appId: "1:101167042392:web:d7002191e9597ac076df2d",
+    measurementId: "G-3520RE961G"
+});
